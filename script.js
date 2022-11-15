86593a8e92c3ec3b14b23fecc81f7e2e28407872
unknown <huydinhvan13@gmail.com>
2022-11-15 21:14:27 +0700

    choose type

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // for (let index = 0; index < item.length; index++) {
 //     const element = item[index];
 // }
-console.log(dataname);
 const hintcombobox = document.getElementsByClassName("combobox-structure-input")[0]
 const input_formula = document.getElementById("structure-input-text")
 const more_choose = document.getElementsByClassName("more-choose")[0]
@@ -17,3 +16,35 @@ hint_formula.forEach(element => {
         hintcombobox.classList.add('combobox-structure-input-hide');
     }
 });
+
+const cbbox = document.querySelectorAll(".generate-column-item select")
+cbbox.forEach(element => {
+    element.onchange = ()=>{
+        console.log(element.value);
+        var node = element.nextElementSibling.querySelector('input[type="text"]')
+        node.value = getFormulaCombobox(element.value)
+    }
+});
+
+function getFormulaCombobox(tenkieu){
+    switch (tenkieu) {
+        case "name":
+            return "[name]"
+            break;
+        case "number":
+            return "[1-100]"
+            break;
+        case "hour":
+            return "[1-24]:[1-60]"
+            break;
+        case "dmy":
+            return "[1-30]/[1-12]/[2010-2022]"
+            break;
+        case "daytime":
+            return "[1-24]:[1-60] [1-30]/[1-12]/[2010-2022]"
+            break;
+        default:
+            return "[]"
+            break;
+    }
+}
