3b647069e157987f8a54c7bd7b521c3fdba2c2c8
unknown <huydinhvan13@gmail.com>
2022-11-15 19:22:57 +0700

    xong phan chon

--- script.js
+++ script.js
@@ -4,7 +4,16 @@
 // }
 console.log(dataname);
 const hintcombobox = document.getElementsByClassName("combobox-structure-input")[0]
+const input_formula = document.getElementById("structure-input-text")
 const more_choose = document.getElementsByClassName("more-choose")[0]
 more_choose.onclick = ()=>{
     hintcombobox.classList.toggle('combobox-structure-input-hide');
 }
+
+const hint_formula = hintcombobox.querySelectorAll(":scope > div")
+hint_formula.forEach(element => {
+    element.onclick = ()=>{
+        input_formula.value = element.querySelector("div").innerHTML
+        hintcombobox.classList.add('combobox-structure-input-hide');
+    }
+});
