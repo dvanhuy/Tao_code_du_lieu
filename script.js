78bed6c33793594cea080400a67f65b46b77af09
unknown <huydinhvan13@gmail.com>
2022-11-16 22:14:07 +0700

    extent cong thuc

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // }
 const hintcombobox = document.getElementsByClassName("combobox-structure-input")[0]
 const input_formula = document.getElementById("structure-input-text")
-const more_choose = document.getElementsByClassName("more-choose")[0]
+const more_choose = document.getElementsByClassName("more-choose-hint")[0]
 more_choose.onclick = ()=>{
     hintcombobox.classList.toggle('combobox-structure-input-hide');
 }
@@ -21,10 +21,19 @@ const cbbox = document.querySelectorAll(".generate-column-item select")
 cbbox.forEach(element => {
     element.onchange = ()=>{
         console.log(element.value);
-        var node = element.nextElementSibling.querySelector('input[type="text"]')
+        var node = element.nextElementSibling.querySelector('.input-fomula')
         node.value = getFormulaCombobox(element.value)
     }
 });
+
+const extent_input = document.querySelectorAll(".more-choose")
+extent_input.forEach(element => {
+    element.onclick = ()=>{
+        console.log(element);
+        element.parentNode.parentNode.classList.toggle("generate-column-item-extend");
+    }
+});
+
 
 function getFormulaCombobox(tenkieu){
     switch (tenkieu) {
